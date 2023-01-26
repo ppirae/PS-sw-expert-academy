e4d17fdde82e5e043198578953037ad0d358f561
HyeongRae Jo <whgudfo77@gmail.com>
2023-01-26 23:49:16 +0900

    PS

--- d3/4406. 모음이 보이지 않는 사람.py
+++ d3/4406. 모음이 보이지 않는 사람.py
@@ -2,13 +2,12 @@ import sys
 sys.stdin = open("input.txt", "r")
 
 T = int(input())
+mo = ['a', 'e', 'i', 'o', 'u']
 for test_case in range(1, T + 1):
-    L, U, X = list(map(int, input().split()))
-    answer = 0
-    if X > U:
-        answer = -1
-    elif L <= X <= U:
-        answer = 0
-    else:
-        answer = L - X
-    print("#" + str(test_case) + " " + str(answer))
+    s = list(input())
+    temp = ''
+    for i in s:
+        if i not in mo:
+            temp += i
+
+    print("#" + str(test_case) + " " + temp)
